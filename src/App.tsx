7ecf91a603bf68b5325f40d8658777f9a8a308e2
Felipe Dobrinin <felipedobrinin@gmail.com>
2024-03-20 22:32:45 -0300

    cosmetic changes

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
 
   function handleButton() {
     addTodo({
-      id: "1",
+      id: Date.now().toString(),
       text: "Algo",
       completed: false,
     } as Todo);
@@ -20,16 +20,20 @@ function App() {
 
   return (
     <div className="app">
-      <h1 className="text-5xl font-bold text-center">TODOs</h1>
-      <SearchInput value={search} setSearch={setSearch} />
-      <p>Filtering by : {search}</p>
-      <button onClick={handleButton}>Add Todo</button>
-      <TodoList
-        todos={todos}
-        addTodo={addTodo}
-        markCompleted={markCompleted}
-        search={search}
-      />
+      <div className="card p-6">
+        <h1 className="text-5xl font-bold text-center">TODOs</h1>
+        <SearchInput value={search} setSearch={setSearch} />
+        <p>Filtering by : {search}</p>
+        <button onClick={handleButton}>Add Todo</button>
+      </div>
+      <main>
+        <TodoList
+          todos={todos}
+          addTodo={addTodo}
+          markCompleted={markCompleted}
+          search={search}
+        />
+      </main>
     </div>
   );
 }
