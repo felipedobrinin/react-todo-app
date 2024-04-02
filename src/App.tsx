861a97cb619558eb090d13e6473d2420faecbc1c
Felipe Dobrinin <felipedobrinin@gmail.com>
2024-04-02 13:43:50 -0300

    feature: implemented removeTodo
    UI : Icon insted of text for add todo buttojn

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,11 @@ import { useTodos } from "@/hooks/useTodos";
 import { TodoList } from "@/components/TodoList";
 import { AddTodoModal } from "@/components/AddTodoModal";
 
+import { GoPlus } from "react-icons/go";
+
 function App() {
   const [search, setSearch] = useState("");
-  const { todos, addTodo, markCompleted } = useTodos();
+  const { todos, addTodo, removeTodo, markCompleted } = useTodos();
   const [openModal, setOpenModal] = useState(false);
   const [showCompleted, setShowCompleted] = useState(true);
 
@@ -23,23 +25,38 @@ function App() {
           <h1 className="text-5xl font-bold text-center">TODOs</h1>
           <SearchInput value={search} setSearch={setSearch} />
           <div className="flex gap-2">
-              <input
-                className="checkbox"
-                type="checkbox"
-                name=""
-                id="show-completed"
-                checked={showCompleted}
-                onChange={() => setShowCompleted(!showCompleted)}
-              />
-              <label htmlFor="show-completed">Show completed todos</label>
+            <input
+              className="checkbox"
+              type="checkbox"
+              name=""
+              id="show-completed"
+              checked={showCompleted}
+              onChange={() => setShowCompleted(!showCompleted)}
+            />
+            <label htmlFor="show-completed">Show completed todos</label>
           </div>
         </div>
-        <TodoList todos={todos} markCompleted={markCompleted} search={search} showCompleted={showCompleted} />
+        <TodoList
+          todos={todos}
+          markCompleted={markCompleted}
+          search={search}
+          showCompleted={showCompleted}
+          removeTodo={removeTodo}
+        />
       </main>
 
-      <button className="add-button text-2xl" onClick={handleButton}>
-        Add Todo
+      <button
+        className={`${openModal ? "bg-danger" : "bg-accent"}
+         add-button text-4xl`}
+        onClick={handleButton}
+      >
+        <GoPlus
+          className={`${
+            openModal ? "rotate-45" : "rotate-0"
+          } transition-transform`}
+        />
       </button>
+
       <AddTodoModal
         addTodo={addTodo}
         openModal={openModal}
