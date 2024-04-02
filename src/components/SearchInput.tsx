e867f5e0cdfb7d1535c29f38a7886cc886593e91
Felipe Dobrinin <felipedobrinin@gmail.com>
2024-04-02 13:44:42 -0300

    feature : ctrl + / for focusing input

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+
 import { Input } from "@/components/base/Input";
 import { FaSearch } from "react-icons/fa";
 
@@ -7,9 +9,30 @@ interface Props {
 }
 
 export const SearchInput: React.FC<Props> = ({ value, setSearch }) => {
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  function handleClick(){
+    inputRef?.current?.focus()
+  }
+
+  const keyDownHandler = (event: KeyboardEvent) => {
+    if (event.ctrlKey && event.key === "/") {
+      console.log("You just pressed ctrl + / for focusing search!");
+      inputRef?.current?.focus();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", keyDownHandler);
+
+    return () => {
+      window.removeEventListener("keydown", keyDownHandler);
+    };
+  });
+
   return (
-    <div className="search-input">
-      <FaSearch className="accent-color icon" />
+    <div className="search-input py-2 px-3 " onClick={handleClick}>
+      <FaSearch className="text-accent"/>
       <Input
         value={value}
         onChange={(e) => {
@@ -17,7 +40,9 @@ export const SearchInput: React.FC<Props> = ({ value, setSearch }) => {
         }}
         type="text"
         placeholder="Search..."
+        ref={inputRef}
       />
+      <span className="text-sm text-zinc-700"><kbd>ctrl</kbd>+<kbd>/</kbd></span>
     </div>
   );
 };
