2458d176b4e8d6269b903407806eecc3fb1f9fd8
Felipe Dobrinin <felipedobrinin@gmail.com>
2024-04-03 12:30:11 -0300

    better style for remove todo button

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,16 +2,20 @@ import { Todo } from "@/lib/Todo";
 
 import { FaRegTrashAlt } from "react-icons/fa";
 
-
 interface Props {
   todo: Todo;
   markCompleted: (id: string) => void;
-  removeTodo : (id: string) => void;
+  removeTodo: (id: string) => void;
 }
 
-export const TodoItem: React.FC<Props> = ({ todo, markCompleted, removeTodo }) => {
+export const TodoItem: React.FC<Props> = ({
+  todo,
+  markCompleted,
+  removeTodo,
+}) => {
   return (
-    <div className="todo-item card px-4 py-4 md:px-6">
+    // <div className="todo-item card py-4 ">
+    <div className="todo-item card pl-4 [&>*]:py-4 md:pl-6">
       <input
         type="checkbox"
         name=""
@@ -23,12 +27,15 @@ export const TodoItem: React.FC<Props> = ({ todo, markCompleted, removeTodo }) =
       <span
         className={`${
           todo.completed ? "line-through text-zinc-400" : ""
-        } text-xl `}
+        } text-xl break-all `}
       >
         {todo.text}
       </span>
-      <button className="remove-todo-button bg-danger hover:scale-105 transition-transform active:scale-100" onClick={() => removeTodo(todo.id)}>
-        <FaRegTrashAlt className="text-neutral-800 text-2xl"/>
+      <button
+        className="remove-todo-button bg-danger"
+        onClick={() => removeTodo(todo.id)}
+      >
+        <FaRegTrashAlt className="text-neutral-800 text-2xl" />
       </button>
     </div>
   );
